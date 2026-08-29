cfa0a7900009cc0a40510187f392a5b30219812e
PANCHU ORAON <jamestoppobit@gmail.com>
2026-08-30 01:31:50 +0530

    Time: 880 ms (5.01%) | Memory: 14.1 MB (50.43%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -2,40 +2,21 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
 
-        sort(nums.begin(), nums.end());
-
         int n = nums.size();
 
-        // Initial closest sum
         int closest = nums[0] + nums[1] + nums[2];
 
         for (int i = 0; i < n - 2; i++) {
 
-            int left = i + 1;
-            int right = n - 1;
+            for (int j = i + 1; j < n - 1; j++) {
 
-            while (left < right) {
+                for (int k = j + 1; k < n; k++) {
 
-                int sum = nums[i] + nums[left] + nums[right];
+                    int sum = nums[i] + nums[j] + nums[k];
 
-                // Update closest answer
-                if (abs(sum - target) < abs(closest - target)) {
-                    closest = sum;
-                }
-
-                // Exact answer found
-                if (sum == target) {
-                    return target;
-                }
-
-                // Sum is too small
-                else if (sum < target) {
-                    left++;
-                }
-
-                // Sum is too large
-                else {
-                    right--;
+                    if (abs(sum - target) < abs(closest - target)) {
+                        closest = sum;
+                    }
                 }
             }
         }
