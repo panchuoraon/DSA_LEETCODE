27b4254a64c3a32f182ca81a1893fcafab76eb00
panchuoraon <jamestoppobit@gmail.com>
2026-08-29 16:42:16 +0530

    Time: 1043 ms (47.05%) | Memory: 7.7 MB (54.76%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -3,7 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
         int fsum=1;
         if(num==1) return false;
-        for(int i=2;i<num;i++){
+        for(int i=2;i<=num/2;i++){
             if(num%i==0) fsum+=i;
         }
         if(fsum==num) return true;
