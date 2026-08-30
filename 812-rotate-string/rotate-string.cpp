696149b2174ea3973f0bfabfa02afa59f95255b8
PANCHU ORAON <jamestoppobit@gmail.com>
2026-08-31 01:27:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (56.11%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -5,21 +5,8 @@ public:
         if (s.length() != goal.length())
             return false;
 
-        int n = s.length();
+        string doubled = s + s;
 
-        for (int i = 0; i < n; i++) {
-
-            if (s == goal)
-                return true;
-
-            // Move first character to the end
-            char first = s[0];
-
-            s.erase(s.begin());
-
-            s.push_back(first);
-        }
-
-        return false;
+        return doubled.find(goal) != string::npos;
     }
 };
