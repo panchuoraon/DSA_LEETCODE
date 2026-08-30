45e42fc48a31afef5b4533b2dcc88307bbacd03d
PANCHU ORAON <jamestoppobit@gmail.com>
2026-08-31 01:42:42 +0530

    Time: 19 ms (26.77%) | Memory: 29 MB (36.39%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int,string ,greater<int>> mp;
+        for(int i=0;i<names.size();i++){
+            mp[heights[i]]=names[i];
+        }
+        vector<string> ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
